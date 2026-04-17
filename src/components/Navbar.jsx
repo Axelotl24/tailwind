35790651b838368352af60e3b2e82769ef9dab3d
Axelotl24 <axelnahuelalvarado@gmail.com>
2026-04-17 12:40:58 -0300

    instalo daisy ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ export default function Navbar() {
             <button className="bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700">
                 Empezar
             </button>
+            <button className="btn btn-primary">Click me!</button>
         </nav>
     );
 }
